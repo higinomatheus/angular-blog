cb27523c9aa2f97fbd70b684be625d63c7b1d08e
Matheus Higino <matheus.higino@yahoo.com.br>
2025-06-07 15:56:18 -0300

    desenhando small-cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
+import { SmallCardComponent } from '../../components/small-card/small-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BigCardComponent, MenuTitleComponent],
+  imports: [BigCardComponent, MenuTitleComponent, SmallCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
