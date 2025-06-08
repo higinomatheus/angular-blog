b9c49c80538952801d74263af24f6507f2cc2320
Matheus Higino <matheus.higino@yahoo.com.br>
2025-06-08 17:53:41 -0300

    desacoplando os componentes da parte visual

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
   imports: [],
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrl: './big-card.component.css',
 })
 export class BigCardComponent {
-
+  @Input()
+  photoCover: string = '';
+  @Input()
+  title: string = '';
+  @Input()
+  description: string = '';
 }
