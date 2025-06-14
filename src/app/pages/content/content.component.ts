b42995de804baa8f6bf6910d7670497df1c9cd07
Matheus Higino <matheus.higino@yahoo.com.br>
2025-06-14 12:39:18 -0300

    pegando os dados dinamicamente

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -8,8 +8,18 @@ import { RouterLink } from '@angular/router';
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
     photoCover: string = "https://lumiere-a.akamaihd.net/v1/images/iron-man_dft_m_db79b94b.jpeg";
     title: string = "MINHA NOTÍCIA";
     description: string = "It works!";
+
+    constructor(private route: ActivatedRoute){
+      this.route.paramMap.subscribe((value) => {
+        console.log(value.get("id"));
+      })
+    }
+
+    ngOnInit(): void {
+
+    }
 }
